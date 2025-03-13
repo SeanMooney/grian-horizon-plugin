d539e2bc2d72f52ef86c524ae87c08698e519883
Sean Mooney <work@seanmooney.info>
2025-03-13 12:31:48 +0000

    swap to chart.js

--- components/time_chart/chart.js
+++ components/time_chart/chart.js
@@ -1,59 +1,61 @@
 // Create a class for the element
 class TimeChart extends HTMLElement {
-    static observedAttributes = ["data", "type"];
+    static observedAttributes = ["data", "type", "options"];
 
     constructor() {
         // Always call super first in constructor
         super();
+        this.attachShadow({mode:'open'});
+        this.shadowRootDiv = document.createElement('div');
+        this.canvas = document.createElement('canvas');
+        this.shadowRootDiv.append(this.canvas);
+        this.shadowRoot.appendChild(this.shadowRootDiv);
+        this.chart = null;
+    }
+
+    init_chart() {
+        const [data, options, type] = this.getInputs();
+        const context = this.canvas.getContext('2d');
+        console.log("render called");
+        this.chart = new Chart(context, {
+            type: type,
+            data: data,
+            options: options
+
+        });
+    }
+
+    getInputs() {
+        return [
+            JSON.parse(this.getAttribute('data')),
+            JSON.parse(this.getAttribute("options") ?? "{}"),
+            this.getAttribute("type") ?? "line"
+         ];
+    }
+
+    update(chart, key, val){
+        if (key === "data"){
+            // when only changing data we do not need to
+            // fully recreate the chart so we just call update
+            chart.data = JSON.parse(val);
+            chart.update();
+        } else {
+            // otherwise if the type or options change we should
+            // do a full recreate
+            this.chart.destroy();
+            this.chart = null;
+            this.init_chart()
+        }
     }
 
     connectedCallback() {
-        const data = this.getAttribute("data");
-        const type = this.getAttribute("renderer") ?? "line";
-        const width = this.getAttribute("width") ?? 400;
-        const height = this.getAttribute("height") ?? 400;
-        const style = this.getAttribute("style") ?? "";
-        const stacked = this.getAttribute("stacked") || false;
-        const stroke = this.getAttribute("stroke") ?? false;
-    
-        const shadow = this.attachShadow({ mode: "open" });
-        
-        
-        const inner_style = document.createElement("style");
-        inner_style.textContent = style;
-        shadow.appendChild(inner_style);
-
-        const wrapper = document.createElement("div");
-        
-        shadow.appendChild(wrapper);
-
-
-        const graph = new Rickshaw.Graph({
-            padding: { top: 0.025, right: 0.025, bottom: 0.025, left: 0.025 },
-            min: "0",
-            stack: false,
-            width: width,
-            height: height,
-            element: wrapper,
-            renderer: type,
-            series: JSON.parse(data)
-        });
-        
-        graph.render();
-
-        
+         this.init_chart();
     }
-
-    disconnectedCallback() {
-        console.log("Custom element removed from page.");
-    }
-
-    adoptedCallback() {
-        console.log("Custom element moved to new page.");
-    }
-
+      
     attributeChangedCallback(name, oldValue, newValue) {
-        console.log(`Attribute ${name} has changed.`);
+        if(this.chart){
+            this.update(this.chart, name, newValue);
+        }
     }
 }
 
